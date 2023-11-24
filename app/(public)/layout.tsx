703c52f78a21b84374c38f437329a86c13858890
Ibrahim Osman <mr.ibrahim@hotmail.se>
2023-11-24 01:37:10 +0100

    bug fix

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -12,12 +12,12 @@ const PublicLayout = async ({ children }: { children: React.ReactNode }) => {
       {/* <div className="h-[80px] md:pl-80 fixed inset-y-0 w-full z-50">
         
       </div> */}
-      <div className=" z-50 sticky top-0 ">
+      {/* <div className=" z-50 sticky top-0 ">
         <div className="absolute">
-          {/* <Header /> */}
+         
           <Navbar />
         </div>
-      </div>
+      </div> */}
 
       {children}
       <Footer />
