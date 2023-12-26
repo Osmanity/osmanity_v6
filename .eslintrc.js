0d723b37c5891894d79e106ea637923df19a9def
Ibrahim Osman <mr.ibrahim@hotmail.se>
2023-12-26 14:30:30 +0100

    lint rule added

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,6 @@ module.exports = {
     "no-shadow-restricted-names": "off",
     "@typescript-eslint/no-unused-vars": "warn",
     "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/ban-types": "off",
   },
 };
