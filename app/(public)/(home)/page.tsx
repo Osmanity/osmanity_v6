8b2b3afbbb06f4bd9910db798e4b232d3145e4ca
Ibrahim Osman <mr.ibrahim@hotmail.se>
2024-01-27 23:44:16 +0100

    home page resp update

--- app/(public)/(home)/page.tsx
+++ app/(public)/(home)/page.tsx
@@ -20,14 +20,14 @@ export default async function Home() {
           playsInline
           muted
           loop
-          className="rotate-180 rounded-2xl absolute top-[-250px]  h-full w-full lg:w-4/5 min-h-[880px] sm:min-h-[1000px] -left-28 lg:-left-14 z-[10]  sm:object-contain  "
+          className="rotate-180 rounded-2xl absolute top-[-250px]  h-full w-full lg:w-4/5 min-h-[940px] sm:min-h-[1000px] -left-28 lg:-left-14 z-[10]  sm:object-contain  "
         >
           {/* <source src="/videos/blackhole.webm" type="video/webm" /> */}
           <source src="/videos/blackhole.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         {/* <div className="absolute top-0 left-0 z-50 w-full min-h-screen bg-black/30 " /> */}
-        <div className="absolute top-0 left-0 z-[12] w-full min-h-[880px] sm:min-h-[1000px] bg-black/50  " />
+        <div className="absolute top-0 left-0 z-[12] w-full min-h-[940px] sm:min-h-[1000px] bg-black/50  " />
         <div className=" w-full ">
           <Hero />
         </div>
