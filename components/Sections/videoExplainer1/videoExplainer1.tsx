e37155af0b35b34d9f44e3f5634874ebe62ce887
Ibrahim Osman <mr.ibrahim@hotmail.se>
2024-02-11 17:02:40 +0100

    update text heading home page

--- components/Sections/videoExplainer1/videoExplainer1.tsx
+++ components/Sections/videoExplainer1/videoExplainer1.tsx
@@ -1,21 +1,33 @@
 const VideoExplainer1 = () => {
   return (
     <section className="md:py-20 py-10 px-4 bg-gradient-to-r from-indigo-900 to-indigo-300 dark:from-black/30 dark:to-black/30 spacey-10 bg-black/30 ">
-      <div className="p-4 sm:p-0 sm:container mx-auto text-start sm:text-center">
-        <div
-          className="text-5xl sm:text-6xl flex justify-center font-bold md:px-20 pb-10
+      <div className=" sm:p-0 sm:container mx-auto text-start sm:text-center">
+        {/*
+        old heading styling
+        className="text-5xl sm:text-6xl flex justify-center font-bold md:px-20 pb-10
             text-gradient
             bg-gradient-to-r
             from-[#c990ff]
             to-[#3e15f3]
             bg-clip-text
             text-transparent
+            " */}
+        <div
+          className="bg-gradient-to-r
+          from-[#c990ff]
+          to-[#3e15f3]
+          bg-clip-text
+          text-transparent
+          text-4xl
+          md:text-5xl
+          font-bold
+          pb-10
             "
         >
           Build a brand and start selling in seconds
         </div>
 
-        <p className="text-lg md:text-xl md-10 text-white/60 bg-clip-text font-bold">
+        <p className="text-lg md:text-xl md-10 text-white bg-clip-text font-bold">
           No matter what you sell, Osmanity has everything you need to build
           your business.
         </p>
